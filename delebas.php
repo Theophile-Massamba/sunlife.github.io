078f05e7ead246dc780c4680717ca62e49a70e29
Theophile-Massamba <118370705+Theophile-Massamba@users.noreply.github.com>
2022-11-30 14:48:39 +0100

    Delete delebas.php

--- delebas.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'connect.php';
-$id=$_REQUEST['id'];
-$query = "DELETE FROM `lvl2_admin` WHERE `lvtwid`=$id";
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: deletesbsad.php");
-?>
