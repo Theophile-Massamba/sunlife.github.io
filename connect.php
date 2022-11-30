793958ee9d89862185e9b70541aad9027fc8c3b4
Theophile-Massamba <118370705+Theophile-Massamba@users.noreply.github.com>
2022-11-30 14:48:16 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$con = mysqli_connect("localhost","root","");
-	date_default_timezone_set ("Asia/Colombo");
-	mysqli_select_db($con,"hospitol");
-	if(!$con){
-			die("Failed to connect");
-			}
-
-?>
