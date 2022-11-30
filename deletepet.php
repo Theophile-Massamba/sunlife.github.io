8ba9062099b6a56c85b716f1574f1652a0f382af
Theophile-Massamba <118370705+Theophile-Massamba@users.noreply.github.com>
2022-11-30 14:49:14 +0100

    Delete deletepet.php

--- deletepet.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('connect.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM `patient` WHERE `pet_id`=$id";
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: supadminop.php");
-?>
