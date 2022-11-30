ec84bfbf0244eb4fb51bd7574b934b754cf5ccb3
Theophile-Massamba <118370705+Theophile-Massamba@users.noreply.github.com>
2022-11-30 14:49:02 +0100

    Delete delesup.php

--- delesup.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'connect.php';
-$id=$_REQUEST['id'];
-$query = "DELETE FROM `sup_admin` WHERE `sadid`=$id";
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: deletesupad.php");
-?>
